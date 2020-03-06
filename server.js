b420c60172e4983171a2522702b81044289a8b8c
Ahmed Salama <eng.ahd746@gmail.com>
2020-03-06 09:18:09 -0500

    v1.0.0

---
+++ server.js
@@ -0,0 +1,27 @@
+//express
+const express = require('express');
+const path =require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, './views/')))
+
+app.get('/',(req , res)=>{
+    res.sendFile(path.join(__dirname, './views/index.html'));
+})
+
+app.get('/about',(req , res)=>{
+    res.sendFile(path.join(__dirname, './dist/about.html'));
+})
+
+app.get('/work',(req , res)=>{
+    res.sendFile(path.join(__dirname, './dist/work.html'));
+})
+
+app.get('/contact',(req , res)=>{
+    res.sendFile(path.join(__dirname, './dist/contact.html'));
+})
+
+
+//Set the port and listen 
+PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
