cf92f4caeca5e3900472666931cb4f10abd4712a
Ahmed Salama <eng.ahd746@gmail.com>
2020-03-15 12:09:28 -0400

    e potofolio

--- server.js
+++ server.js
@@ -3,24 +3,7 @@ const express = require('express');
 const path =require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, './dist')))
-
-app.get('/',(req , res)=>{
-    res.sendFile(path.join(__dirname, './dist/index.html'));
-})
-
-app.get('/about',(req , res)=>{
-    res.sendFile(path.join(__dirname, './dist/about.html'));
-})
-
-app.get('/work',(req , res)=>{ 
-    res.sendFile(path.join(__dirname, './dist/work.html'));
-})
-
-app.get('/contact',(req , res)=>{
-    res.sendFile(path.join(__dirname, './dist/contact.html'));
-})
-
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('/resume',(req , res)=>{
     res.sendFile(path.join(__dirname, './dist/resume.pdf'));
